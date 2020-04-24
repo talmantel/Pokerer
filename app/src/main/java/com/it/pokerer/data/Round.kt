28d06604897a6565efdf082d366ab0fdde10a42f
Tal Man <tal.mantelmakher@gmail.com>
2020-04-24 13:53:38 +0300

    Optimize code

--- app/src/main/java/com/it/pokerer/data/Round.kt
+++ app/src/main/java/com/it/pokerer/data/Round.kt
@@ -7,5 +7,4 @@ import androidx.room.PrimaryKey
 data class Round (@PrimaryKey val ts: String,
              val gilWon: Int,
              val talWon: Int,
-             val shayWon: Int){
-}
+             val shayWon: Int)
